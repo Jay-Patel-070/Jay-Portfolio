08430c4141d66040640e0405657a53aa5975476a
Jay <jayspatel420005@gmail.com>
2025-07-07 11:57:59 +0530

    made external link working

--- lib/external_links.dart
+++ lib/external_links.dart
@@ -1,30 +1,45 @@
-import 'dart:html' as html;
+import 'dart:io' show Platform; // For mobile platform check
 import 'package:flutter/foundation.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+/// Only import dart:html if running on web:
+/// To avoid build errors for mobile apps, this import should be conditional.
+/// But if you want simplest, just keep it and run on web.
+import 'dart:html' as html;
 
 class ExternalLinks {
-  static void openWebUrl(String url) {
+  static Future<void> openUrl(String url) async {
     if (kIsWeb) {
+      // For web, open in new tab/window
       html.window.open(url, '_blank');
+    } else {
+      // For mobile platforms, use url_launcher
+      final Uri uri = Uri.parse(url);
+      if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+        throw 'Could not launch $url';
+      }
     }
   }
 
-  static void downloadCv() {
-    openWebUrl('https://drive.google.com/uc?export=download&id=1MRjLhemT-u_9Xi6xP2aOmv3MB0LV81X4');
+  static Future<void> downloadCv() async {
+    await openUrl(
+      'https://drive.google.com/uc?export=download&id=1MRjLhemT-u_9Xi6xP2aOmv3MB0LV81X4',
+    );
   }
 
-  static void openInstagram() {
-    openWebUrl('https://www.instagram.com/jay_070_1?igsh=MW1vYnZtdW0yd2RqeA==');
+  static Future<void> openInstagram() async {
+    await openUrl('https://www.instagram.com/jay_070_1?igsh=MW1vYnZtdW0yd2RqeA==');
   }
 
-  static void openLinkedIn() {
-    openWebUrl('https://www.linkedin.com/in/jay-patel-34758b272');
+  static Future<void> openLinkedIn() async {
+    await openUrl('https://www.linkedin.com/in/jay-patel-34758b272');
   }
 
-  static void openWhatsapp() {
-    openWebUrl('https://wa.me/7046881990');
+  static Future<void> openWhatsapp() async {
+    await openUrl('https://wa.me/7046881990');
   }
 
-  static void openGitHub() {
-    openWebUrl('https://github.com/Jay-Patel-070');
+  static Future<void> openGitHub() async {
+    await openUrl('https://github.com/Jay-Patel-070');
   }
 }
